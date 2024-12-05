9f615eea5de798fc116ec78e4917c990ebbe37a7
Nick de Jong <d00469548@utahtech.edu>
2024-12-04 17:31:36 -0700

    First Submission

--- features/HeronsCalculator.feature
+++ features/HeronsCalculator.feature
@@ -3,7 +3,22 @@ As an aspiring mathematician
 I should be able to calculate the area of a triangle
 So that I can chat with my math friends like a pro
 
-Scenario: I can calculate the area of a triangle
-    Given I open the url "https://byjus.com/herons-calculator/"
-    # write your steps here
+ Background:
+   Given I open the url "https://byjus.com/herons-calculator/"
+   And I pause for 500ms
+
+ Scenario: I can calculate the area of a triangle
+   When I set "20" to the inputfield "#a"
+   And I set "26" to the inputfield "#b"
+   And I set "7" to the inputfield "#c"
+   And I click on the button ".clcbtn"
+   Then I expect that element "#_d" contains the text "40.981"
+
+ Scenario: I can use bad inputs and not crash the website
+   When I set "20" to the inputfield "#a"
+   And I set "20" to the inputfield "#b"
+   And I set "41" to the inputfield "#c"
+   And I click on the button ".clcbtn"
+   Then I expect that a alertbox is opened
+    
 
--- test_battery.py
+++ test_battery.py
@@ -14,13 +14,78 @@ def partially_charged_battery():
     b.mCharge = 70
     return b
 
+@pytest.fixture
+def monitored_battery(mocker):
+    return Battery(100, external_monitor=Mock())
+
 
 def describe_Battery():
 
+    def describe_init():
+        def describe_battery_initializes_without_monitor():
+            def capacity_initialized(charged_battery):
+                assert charged_battery.mCapacity == 100
+
+            def charge_initialized_to_capacity(charged_battery):
+                assert charged_battery.mCharge == 100
+            
+            def monitor_initialized_to_none(charged_battery):
+                assert charged_battery.external_monitor == None
+
+        def describe_battery_initializes_with_monitor():
+            def capacity_initialized(monitored_battery):
+                assert monitored_battery.mCapacity == 100
+
+            def charge_initialized_to_capacity(monitored_battery):
+                assert monitored_battery.mCharge == 100
+            
+            def monitor_initialized(monitored_battery):
+                assert isinstance(monitored_battery.external_monitor, Mock)
+
+    def describe_get_capacity():
+        def battery_returns_capacity_when_fully_charged(charged_battery):
+            assert charged_battery.getCapacity() == charged_battery.mCapacity == 100
+        
+        def battery_returns_capacity_when_partially_charged(partially_charged_battery):
+            assert partially_charged_battery.getCapacity() == partially_charged_battery.mCapacity == 100
+
+    def describe_get_charge():
+        def battery_returns_charge_when_fully_charged(charged_battery):
+            assert charged_battery.getCharge() == charged_battery.mCharge == 100
+        
+        def battery_returns_charge_when_partially_charged(partially_charged_battery):
+            assert partially_charged_battery.getCharge() == partially_charged_battery.mCharge == 70
+
     def describe_recharge():
-        # your test cases here
+        def battery_recharges_succesfully(partially_charged_battery):
+            assert partially_charged_battery.recharge(20) is True
+            assert partially_charged_battery.getCharge() == 90
+        
+        def battery_does_not_recharge_past_capacity(charged_battery, partially_charged_battery):
+            assert charged_battery.recharge(10) is False
+            assert charged_battery.getCharge() == 100
+
+            assert partially_charged_battery.recharge(40) is True
+            assert partially_charged_battery.getCharge() == 100
+        
+        def battery_notifies_recharge_to_monitor(monitored_battery):
+            monitored_battery.mCharge = 70
+            monitored_battery.recharge(20)
+            monitored_battery.external_monitor.notify_recharge.assert_called_once_with(90)
 
     def describe_drain():
-        # your test cases here
+        def battery_drains_succesfully(charged_battery):
+            assert charged_battery.drain(30) is True
+            assert charged_battery.getCharge() == 70
+        
+        def battery_does_not_drain_past_capacity(charged_battery):
+            assert charged_battery.drain(150) is True
+            assert charged_battery.getCharge() == 0
 
+            assert charged_battery.drain(10) is False
+            assert charged_battery.getCharge() == 0
+        
+        def battery_notifies_drain_to_monitor(monitored_battery):
+            monitored_battery.drain(30)
+            monitored_battery.external_monitor.notify_drain.assert_called_once_with(70)
 
